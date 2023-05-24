2f44307ac3f92e7f5016a8a294a8cf05e51537d3
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-24 17:22:59 +0300

    feat: Import Home Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
+import { Home } from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header bg-black bg-blend-darken">
-        <h1 className='text-4xl text-slate-300 animate-pulse'> React Template Repository with the following packages Pre-installed :</h1>      
-        <ol className='pt-5'>
-           <li>React Router (v5) </li>
-           <li>Tailwind CSS </li>
-            <li> Configuration to use a proxy server.</li>
-            <li>React start script modified to use openssl legacy provider.</li>
-        </ol>
-      
-      </header>
+    <div 
+      className
+      ="text-center text-2xl">
+     <Home />
     </div>
   );
 }
