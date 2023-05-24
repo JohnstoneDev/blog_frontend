01b65f3d5f42a17a6268e7e29c8acdde127d7788
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-24 17:23:24 +0300

    feat: Create Home Component

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+
+export function Home(){
+	return (
+		<>
+			<div className="flex flex-col gap-6 items-center">
+				<h1 className="font-headings"> This is the Home page of my blog</h1>
+				<p className="font-text"> This is a sample entry </p>
+				<p className="font-info">This is sample information(links etc)</p>
+			</div>
+		</>
+	)
+}
