a683a976200fc9abc9f80c91a66c13153b67ef84
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-24 22:36:50 +0300

    style: Workout component display

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,13 @@
-
 export function Home(){
 	return (
-		<>
-			<div className="flex flex-col gap-6 items-center">
-				<h1 className="font-headings"> This is the Home page of my blog</h1>
-				<p className="font-text"> This is a sample entry </p>
-				<p className="font-info">This is sample information(links etc)</p>
+			<div
+				className="flex flex-col gap-6 items-center p-2 w-full h-full font-thin">
+				<div className="p-2 m-2">
+						<h1> This is the Home page of the blog</h1>
+						<div className="h-full w-full flex flex-col gap-6">
+							<h3 className="font-text"> Sample Content</h3>
+						</div>
+				</div>
 			</div>
-		</>
 	)
 }
