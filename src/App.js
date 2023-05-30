eaa235b7c293b8065b0d49e8cd554a92c8ae6b6e
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-30 14:48:08 +0300

    fix: Move some logic into Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import { useEffect, useState } from 'react';
-
 import './App.css';
 import { Paths }  from '../src/Components/Routes'; 
 
 function App() {
-  const [ message, setMessage ] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:3000/')
-    .then(r => r.json())
-    .then(d => setMessage(d.message))
-    .catch(e => console.log(e.message))
-  },[])
 
   return (
     <div
       className
       ="text-center text-2xl bg-[#0B0C10] font-content text-[#D1E8E2] min-h-screen w-screen bg-cover">
       <div>
-       <h1 className='text-4xl p-2 mb-2 font-headings'>{message}</h1>
        <Paths  />
       </div>
     </div>
