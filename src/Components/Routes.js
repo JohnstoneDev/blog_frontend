e39adeb51388063ae23ee5f897b621d98c794247
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-30 14:30:22 +0300

    feat: SingleStory Path

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from './Home';
+import { SingleStory } from "./SingleStrory";
 
 
 export function Paths()
@@ -8,6 +9,7 @@ export function Paths()
 	return (
 		<Routes>
 				<Route path='/' element={<Home />} />
+				<Route path='/story/:id' element={<SingleStory/>} />
 		</Routes>
 	)
 }
