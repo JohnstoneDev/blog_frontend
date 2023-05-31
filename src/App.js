0dc4c8b76f7b06eae9b1e3e7f96bc99ad0807cfe
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-31 16:33:34 +0300

    style: Hide y-overflow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div
       className
       ="text-center text-2xl bg-[#1f1a1a] font-content text-[#cecece] min-h-screen
-      w-screen bg-cover leading-loose antialiased tracking-wide">
+      min-w-screen bg-cover leading-loose antialiased tracking-wide overflow-y-hidden">
       <div>
        <Paths  />
       </div>
