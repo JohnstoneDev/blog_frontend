ce8021c5640c28e120bb745227ba92781d443a2e
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-31 16:26:07 +0300

    style: Appearance of the stories Page

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -1,29 +1,41 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom'
 
+import { FcCalendar } from 'react-icons/fc';
+
 import { StoriesContext } from "./Home";
 
 const DisplayStory = ({ id, title, story_summary, created_at, topic }) => {
-	const written_date = new Date(created_at).toUTCString();
+	const written_date = new Date(created_at).toLocaleDateString();
 
 	return (
-		<div
-			className
-			='flex flex-col gap-4 justify-center align-middle m-2 p-2'>
-			<section className='flex gap-4'>
-				<h2 className='font-headings mb-2'>{title}</h2>
-				<h4>Published : {written_date}</h4>
-			</section>
+		<div>
+					<div
+						className
+						='flex flex-col gap-4 justify-end align-middle
+						 rounded cursor-pointer shadow-lg shadow-neutral-700 p-4 w-full box-border
+						 hover:shadow-2xl hover:shadow-neutral-700'>
 
-			<section>
-				<h4>{story_summary}</h4>
-				<h5 className='font-semibold'>{topic.name}</h5>
-			</section>
+						<section className='flex gap-4 w-full font-headings space-x-4 justify-between'>
+							<h2 className='font-headings text-4xl mb-2'>{title}</h2>
+							<h4 className='underline text-lg flex space-x-4 gap-4 items-center justify-end'> 
+								<FcCalendar />{written_date}
+							</h4>
+						</section>
 
-			<Link to={`/story/${id}`}>
-				View Full Story
-			</Link>
-		</div>
+						<section className='flex flex-col gap-4'>
+							<h4>{story_summary}</h4>
+							<h5 className='font-bold underline tracking-widest text-blue-500 font-headings'>{topic.name}</h5>
+								<Link
+									to={`/story/${id}`}
+									className='hover:text-blue-500'
+									>
+									View Full Story
+								</Link>
+						</section>
+
+					</div>
+				</div>
 	)
 }
 
@@ -32,16 +44,18 @@ export function Stories(){
 	const context = useContext(StoriesContext);
 
 	return (
-		<div 
-			className
-			='grid grid-cols-2 gap-8 text-left items-center align-middle justify-center w-full
-			m-2 p-2'>
-			{
-				context.stories.map((story) => {
-					return <DisplayStory key={story.id} {...story} />
-				})
-			}
-		</div>
+		<div>
+			<div
+				className
+				='grid gap-8 text-left items-center align-middle justify-center w-full
+				m-4 p-2'>
+				{
+					context.stories.map((story) => {
+						return <DisplayStory key={story.id} {...story} />
+					})
+				}
+			</div>
+	</div>
 	)
 }
 
