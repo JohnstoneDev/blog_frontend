c2f7f1bcfc81d40920e372af8757f422b0de3a25
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-31 16:24:36 +0300

    style: Background & Text color, font styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import { Paths }  from '../src/Components/Routes'; 
+import { Paths }  from '../src/Components/Routes';
 
 function App() {
 
   return (
     <div
       className
-      ="text-center text-2xl bg-[#0B0C10] font-content text-[#D1E8E2] min-h-screen w-screen bg-cover">
+      ="text-center text-2xl bg-[#1f1a1a] font-content text-[#cecece] min-h-screen
+      w-screen bg-cover leading-loose antialiased tracking-wide">
       <div>
        <Paths  />
       </div>
