1dd4bdebecf9563e9ec374fabac230945b7eda28
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-02 00:46:32 +0300

    feat: Component to display when there is no data from the API

---
+++ src/Components/NoDisplay.js
@@ -0,0 +1,19 @@
+
+export const NoDisplayData = () => {
+  return (
+    <div className='m-4 p-4 font-headings text-left'>
+      <h1> Hello There !</h1>
+      <section className="p-2 m-2 text-left">
+        <p>
+          Seems like your are early to the party on THE MERMAID TARVERN.
+          I have not published here yet and still write on <a href="https://medium.com/@maruchajohnstone" target='_blank' rel="noreferrer" className='text-blue-500 underline'>Medium</a>.
+        </p>
+          <p>
+            I will update you once this site is up !
+          </p>
+      </section>
+			<h2>THANKS !</h2>
+			<h2> Marucha Johnstone.</h2>
+    </div>
+  )
+}
