e366c06540f2f99712be990aceba57659fb52685
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-01 17:42:38 +0300

    style: Add some links to the navigation & style them

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -12,13 +12,14 @@ export const Navigation = () => {
   },[])
 
 	return (
-		<div className="font-headings text-3xl grid align-middle items-center">
+		<div className="font-headings text-3xl grid align-middle items-center p-2">
 			<Link className='text-6xl p-4 m-4 font-headings font-extrabold' to='/'>
 				{message}
 			</Link>
 
-			<div>
-					<Link to="/topics"> Topics </Link>
+			<div className="flex items-center justify-around">
+					<Link to='/topics'> Topics </Link>
+					<Link to="/about"> About </Link>
 			</div>
 		</div>
 	)
