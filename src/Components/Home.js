698358dd49bac3e99c65ba5f149715c3463f093a
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-02 00:47:37 +0300

    feat: Import context & use for conditional rendering

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 
-
+import { GlobalContext } from "../App";
 import { Stories } from "./Stories";
 import { Navigation } from "./Navigation";
+import { NoDisplayData } from "./NoDisplay";
 
 export const convertDate = (date_input) => {
 	return new Date(date_input).toLocaleString();
@@ -21,11 +23,21 @@ export const BackButton = () => {
 }
 
 export function Home(){
+	const context = useContext(GlobalContext);
+
+	const stories = context.stories;
 
 	return (
 		<div className="min-h-screen p-2">
+			{
+				stories.length > 0 ?
+			<div>
 				<Navigation />
 				<Stories />
+			</div>
+			:
+			<NoDisplayData />
+			}
 		</div>
 	)
 };
