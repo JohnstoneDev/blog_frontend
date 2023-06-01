3492656e9802d989aa215b0c0c72cf89c0306d1d
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-01 18:21:49 +0300

    fix: Remove unnecessary comm with Backend

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,20 +1,11 @@
-import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
-	const [ message, setMessage ] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:3000/')
-    .then(r => r.json())
-    .then(d => setMessage(d.message))
-    .catch(e => console.log(e.message))
-  },[])
 
 	return (
 		<div className="font-headings text-3xl grid align-middle items-center p-2">
 			<Link className='text-6xl p-4 m-4 font-headings font-extrabold' to='/'>
-				{message}
+				THE MERMAID TARVERN 
 			</Link>
 
 			<div className="flex items-center justify-around">
